d736b1a99dbb8f69d758d3ab4b6c5a675c69462b
Prajwal Agrawal <findprajju@gmail.com>
2026-07-14 15:01:52 -0700

    Initial Commit

---
+++ macTablaPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macTablaPro
+//
+//  Created by Prajwal Agrawal on 7/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macTablaPro/macTablaProApp.swift
@@ -0,0 +1,17 @@
+//
+//  macTablaProApp.swift
+//  macTablaPro
+//
+//  Created by Prajwal Agrawal on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct macTablaProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
