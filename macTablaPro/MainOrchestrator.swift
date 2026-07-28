77fa25643586af189c3f771cffbfc45b61db446d
Prajwal Agrawal <findprajju@gmail.com>
2026-07-28 10:18:15 -0700

    Auto-start instruments on preset selection based on preset On/Off flags and persist mixer volume in preset definitions

--- macTablaPro/MainOrchestrator.swift
+++ macTablaPro/MainOrchestrator.swift
@@ -167,16 +167,25 @@ class AppAudioOrchestrator: ObservableObject {
             tb.tempoBPM = preset.Tempo
             tb.volume = preset.TablaGain
             tb.useSurTabla = preset.UseSurTabla
+            if preset.TablaOn != tb.isPlaying {
+                tb.togglePlay()
+            }
         }
 
         if let t1 = self.tanpura1 {
             t1.volume = preset.Tanpura1Gain
             t1.firstStringPitch = ITablaProPreset.stringNameToCents(preset.Tanpura1FirstString)
+            if preset.Tanpura1On != t1.isPlaying {
+                t1.togglePlay()
+            }
         }
 
         if let t2 = self.tanpura2 {
             t2.volume = preset.Tanpura2Gain
             t2.firstStringPitch = ITablaProPreset.stringNameToCents(preset.Tanpura2FirstString)
+            if preset.Tanpura2On != t2.isPlaying {
+                t2.togglePlay()
+            }
         }
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
